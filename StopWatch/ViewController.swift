e69b2745debf33ddedd46f460f70dead3aed9a3b
D7703_13 <d7703_13@d7703-13ui-imac.local>
2018-04-09 16:12:12 +0900

    123

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,22 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var animalLabel: UILabel!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var startButton: UIButton!
     @IBOutlet var stopButton: UIButton!
     @IBOutlet var resetButton: UIButton!
     
-    var timer =  Timer()
+    var timer = Timer()
     var count = 0
-    var animals = ["🐭","🐮","🐯","🐰","🐲","🐍","🐴","🐏","🐒","🐓","🐕","🐗"]
-    var animalIndex = 0
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        changeButoonState(start: true, stop: false, reset: false)
 
     }
 
@@ -37,7 +33,6 @@ class ViewController: UIViewController {
     
     
     @IBAction func tappedStart(sender: UIButton) {
-        changeButoonState(start: false, stop: true, reset: false)
         timer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: { (timer) in
             self.update()
         })
@@ -45,45 +40,27 @@ class ViewController: UIViewController {
     
     
     @IBAction func tappedStop(sender: UIButton) {
-        changeButoonState(start: true , stop: false, reset: true)
         timer.invalidate()
     }
     
     
     @IBAction func tappedReset(sender: UIButton) {
-        changeButoonState(start: true, stop: false, reset: false)
         timer.invalidate()
         count = 0
         timeLabel.text = "00:00:00"
-        animalIndex = 0
-        animalLabel.text = animals[0]
+        
     }
     
     
     func update() {
         // count
         count += 1
-        // animal
-        if count % 1200 == 0 {
-            animalIndex = 0
-            animalLabel.text = animals[0]
-        }
-        else if count % 100 == 0 {
-            animalIndex += 1
-            animalLabel.text = animals[animalIndex]
-        }
+        
         // time
         let min = count / 60 / 100
         let sec = (count - (min * 60 * 100)) / 100
         let msec = count - (min * 60 * 100) - (sec * 100)
         timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
-    }
-    
-    
-    func changeButoonState(start: Bool, stop: Bool, reset: Bool) {
-        startButton.isEnabled = start
-        stopButton.isEnabled = stop
-        resetButton.isEnabled = reset
     }
     
 }
